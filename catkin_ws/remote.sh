7fee8c665ecf9c9eeacf1ed4db25f9585d5bc935
Eldar Abibulaev <abibulaev.eldar@gmail.com>
2022-04-01 11:24:39 +0300

    Scripts launch PATH fix. Build deps fix. Env script fix.

--- catkin_ws/remote.sh
+++ catkin_ws/remote.sh
@@ -3,8 +3,10 @@ if [[ "$#" != "1" ]]
 then
     echo "using: $0 <bot_name>"
 else
-	source ~/git/Software/catkin_ws/devel/setup.bash
-	export ROS_MASTER_URI=http://$1:11311
-	export ROS_MASTER=duckpi4
-	export VEHICLE_NAME=duckpi4
+	source devel/setup.bash
+
+	export ROS_HOSTNAME=10.135.4.155
+	export ROS_MASTER_URI=http://$1.local:11311
+	export ROS_MASTER=$1
+	export VEHICLE_NAME=$1
 fi
