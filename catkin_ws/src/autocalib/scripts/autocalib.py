4c13a4a58e62c8ac6a77d856e0626bc6b5ccdc3f
Eldar Abibulaev <abibulaev.eldar@gmail.com>
2022-05-13 13:59:40 +0300

    added new state system. New autocalib method realized

--- catkin_ws/src/autocalib/scripts/autocalib.py
+++ catkin_ws/src/autocalib/scripts/autocalib.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
 import os
+import time
+
 import rospy
 from duckietown_msgs.msg import Twist2DStamped, WheelsCmdStamped
 from sensor_msgs.msg import CompressedImage
 import cv2
 import numpy as np
+import apriltag
+
+
+class my_state:
+    def __init__(self, board_found=False, board_num=-1, is_going_to_right=True):
+        self.board_found = board_found
+        self.board_num = board_num
+        self.is_going_to_right = is_going_to_right
+        self.checked = False
+
+    def __str__(self):
+        return "Boards found: %s\nBoard num: %d\nGoing rigt?: %s\nChecked?: %s" % (
+            "Yes" if self.board_found else "No", self.board_num, "Yes" if self.is_going_to_right else "No",
+            self.checked)
+
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((5*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
+objp = np.zeros((5 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:5].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints = {}  # 3d point in real world space
+imgpoints = {}  # 2d points in image plane.
 
 
 def bgr_from_jpg(data):
@@ -28,6 +45,21 @@ def bgr_from_jpg(data):
         raise ValueError(msg)
     return bgr
 
+
+def calib_test(img, mtx, dist):
+    # undistort
+    dst = cv2.undistort(img, mtx, dist)
+    # crop the image
+    # x, y, w, h = roi
+    # dst = dst[y:y + h, x:x + w]
+
+    cv2.imshow("test", img)
+    cv2.imshow("test1", dst)
+    cv2.waitKey(0)
+
+    return dst
+
+
 class MyNode():
 
     def __init__(self, node_name):
@@ -35,91 +67,91 @@ class MyNode():
         self.img = None
         self.pub = rospy.Publisher("~wheels_cmd", WheelsCmdStamped, queue_size=1)
         self.sub_image = rospy.Subscriber("~image/compressed", CompressedImage, self.processImage, queue_size=1)
+        self.gray = None
+        options = apriltag.DetectorOptions(families="tag36h11")
+        self.detector = apriltag.Detector(options)
+
+    def detectTags(self):
+        return self.detector.detect(self.gray)
 
     def processImage(self, image_msg):
-        print('processImage()')
+        # print('processImage()')
         self.img = bgr_from_jpg(image_msg.data)
-    
-    def step(self, cw = True):
+        self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+    def step(self, is_going_to_right=True):
         msg = WheelsCmdStamped()
-        rate = rospy.Rate(10) # 1Hz
-        msg.vel_left = -0.3
-        msg.vel_right = 0.3
-        if cw:
+        rate = rospy.Rate(0.5)  # 1Hz
+        rate2 = rospy.Rate(10)
+        msg.vel_left = -0.2
+        msg.vel_right = 0.2
+        if is_going_to_right:
             msg.vel_left, msg.vel_right = msg.vel_right, msg.vel_left
-        rospy.loginfo("Publishing message")
+        # rospy.loginfo("Publishing message")
         self.pub.publish(msg)
-        rate.sleep()
-        msg.vel_left =  0.0
+        rate2.sleep()
+        msg.vel_left = 0.0
         msg.vel_right = 0.0
-        rospy.loginfo("Publishing message -")
+        # rospy.loginfo("Publishing message -")
         self.pub.publish(msg)
-        rate.sleep()
         rate.sleep()
 
+    def is_chessboard_found(self, img):
+        ret, corners = cv2.findChessboardCorners(self.gray, (7, 5), None)
+        return ret == True
+
     def handle_img(self, img):
-        self.gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(self.gray, (7,5),None)
+        ret, corners = cv2.findChessboardCorners(self.gray, (7, 5), None)
         if ret == True:
             all_x = [pair[0][0] for pair in corners]
-            objpoints.append(objp)
-            cv2.cornerSubPix(self.gray,corners,(11,11),(-1,-1),criteria)
-            imgpoints.append(corners)
-            cv2.drawChessboardCorners(img, (7,5), corners,ret)
+            objpoints[tuple(corners.tobytes())] = objp
+            cv2.cornerSubPix(self.gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints[tuple(corners.tobytes())] = corners
+            cv2.drawChessboardCorners(img, (7, 5), corners, ret)
             return (min(all_x), max(all_x))
         return None
 
-    def fsm(self, state, board):
-        if board > 1:
-            return (True,  False, True ), board-1
-        if board < 0:
-            return (False,  False, True ), board+1
-        rules = {
-            # cw,   isVis, restor
-            (False, True,  False): ((False, True,  False), board  ),  # normal CCW
-            (True,  True,  False): ((True,  True,  False), board  ),  # normal CW
-            (False, False, False): ((False, False, True ), board+1),  # insiv when CCW -> go CW back
-            (True,  False, False): ((True,  False, True ), board-1),  # insiv when CW -> go CCW back
-            
-            # (False, False, False): ((True,  False, True ), !)  # insiv when CCW -> go CW back
-            # (True,  False, False): ((False, False, True ), !)  # insiv when CW -> go CCW back
-            (True,  False, True ): ((True,  False, True ), board  ),  # normal CW restoration
-            (False, False, True ): ((False, False, True ), board  ),  # normal CCW restoration
-            (True,  True,  True ): ((True,  True,  False), board  ),  # restores, now normal CW movement
-            (False, True,  True ): ((False, True,  False), board  )  # restores, now normal CCW movement
-        }
-        return rules[state][0], rules[state][1]
-
     def run(self):
         cw = True
-        rate = rospy.Rate(1.5) # 1Hz
+        rate = rospy.Rate(1)  # 1Hz
         dir_changes = 0
-        board = 0 # CCW from 0
+        board = 0  # CCW from 0
         # cw, isVis, restor
-        state = (cw, True, False)
+        state = my_state(board_found=cw, board_num=board, is_going_to_right=True)
         while not rospy.is_shutdown():
-            if self.img is not None:
-                img = self.img
-                res = self.handle_img(img)
-                print(res)
-                print(state)
-                if res is None:
-                    state = (state[0], False, state[2])
+            if (self.img is not None) and (self.gray is not None):
+                if self.is_chessboard_found(self.img):
+                    state.board_found = True
+                    if not state.checked:
+                        state.board_num += 1 if state.is_going_to_right == True else -1
+                        state.checked = True
+                        res = self.handle_img(self.img)
+                        print(res)
+
                 else:
-                    state = (state[0], True, state[2])
-                new_state, board = self.fsm(state, board)
-                if state[0] != new_state[0]:
-                    dir_changes += 1
-                state = new_state    
+                    state.board_found = False
+                    if ((state.board_num <= -1) or (state.board_num >= 1)) and state.checked:
+                        state.is_going_to_right = not state.is_going_to_right
+                        state.checked = False
+                        dir_changes += 1
+                    elif (state.board_num == 0) and (dir_changes == 2):
+                        dir_changes += 1
+                    elif state.board_num == 0:
+                        state.checked = False
                 print(state)
-                self.step(state[0])
-                cv2.imshow('img', img)
+                print(dir_changes)
+                self.step(state.is_going_to_right)
+                cv2.imshow('img', self.img)
                 cv2.waitKey(500)
-                if dir_changes >=2:
+                if dir_changes >= 3:
                     break
         cv2.destroyAllWindows()
         self.sub_image.unregister()
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, self.gray.shape[::-1],None,None)
+        print(len(list(objpoints.values())))
+        print(len(objpoints.values()), len(imgpoints.values()))
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(list(objpoints.values()), list(imgpoints.values()),
+                                                           self.gray.shape[::-1],
+                                                           None, None)
         print('cv2.calibrateCamera: ')
         print('mtx = ', mtx)
         print('dist = ', dist)
@@ -127,20 +159,23 @@ class MyNode():
         print('tvecs = ', tvecs)
         h = 480
         w = 640
-        newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0.1,(w,h))
-        mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-        dst = cv2.remap(self.img,mapx,mapy,cv2.INTER_LINEAR)
-        cv2.imwrite('/tmp/calibresult.png',dst)
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 0.1, (w, h))
+        mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
+        dst = cv2.remap(self.img, mapx, mapy, cv2.INTER_LINEAR)
+        cv2.imwrite('/tmp/calibresult.png', dst)
 
         mean_error = 0
         for i in range(len(objpoints)):
-            imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-            error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+            imgpoints2, _ = cv2.projectPoints(list(objpoints.values())[i], rvecs[i], tvecs[i], mtx, dist)
+            error = cv2.norm(list(imgpoints.values())[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
             mean_error += error
-        print("total error: ", mean_error/len(objpoints))
+        print("total error: ", mean_error / len(objpoints))
         rospy.signal_shutdown('calibration done')
 
+        calib_test(self.img, mtx, dist)
 
+
+start = time.time()
 if __name__ == '__main__':
     # create the node
     node = MyNode(node_name='autocalib_node')
@@ -148,3 +183,4 @@ if __name__ == '__main__':
     node.run()
     # keep spinning
     rospy.spin()
+print("finished in " + str(time.time() - start))
