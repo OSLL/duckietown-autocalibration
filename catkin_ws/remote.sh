393e6aeb3d0ddf69cf93e1e6a5627ef015c268b9
Eldar Abibulaev <abibulaev.eldar@gmail.com>
2022-05-13 13:59:09 +0300

    remote.sh auto ROS_HOSTNAME network ip detect

--- catkin_ws/remote.sh
+++ catkin_ws/remote.sh
@@ -5,7 +5,7 @@ then
 else
 	source devel/setup.bash
 
-	export ROS_HOSTNAME=10.135.4.73
+	export ROS_HOSTNAME=`ip route | grep -Eom 1 "([0-9]{1,3}[\.]){3}[0-9]{1,3}" -C 1`
 	export ROS_MASTER_URI=http://$1.local:11311
 	export ROS_MASTER=$1
 	export VEHICLE_NAME=$1
